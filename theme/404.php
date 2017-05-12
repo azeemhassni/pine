bcbc1c1ca8b7f6c4daefa8aaf907d4643efa780f
usamamuneerchaudhary <info@usamamuneer.me>
2017-05-12 12:21:23 +0500

    Created the 404 pages for the theme

---
+++ theme/404.php
@@ -0,0 +1,9 @@
+<?php
+use Timber\Timber;
+/**
+ * Default 404 Page
+ */
+
+$context = Timber::get_context();
+
+Timber::render('views/pages/404.twig',$context);
---
+++ theme/GulpFile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var cleancss = require('gulp-clean-css');
+var imgmin = require('gulp-imagemin');
+
+/**
+ * Styles Task
+ */
+gulp.task('styles',function(){
+    return gulp.src(['resources/sass/style.scss'])
+        .pipe(sass())
+        .pipe(cleancss())
+        .pipe(concat('main.css'))
+        .pipe(rename({suffix : '.min'}))
+        .pipe(gulp.dest('assets/css/'))
+});
+
+/**
+ * Images Task
+ */
+gulp.task('images', function () {
+    return gulp.src(['resources/images/**/*'])
+        .pipe(imgmin())
+        .pipe(gulp.dest('assets/images/'));
+});
+
+/**
+ * Scripts Task
+ */
+gulp.task('scripts', function(){
+    return gulp.src(['resources/libs/**/*.js'])
+        .pipe(concat('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js/'))
+
+});
+
+/**
+ * Watch Task
+ */
+gulp.task('watch', function () {
+    gulp.watch('resources/sass/**/*.scss', ['styles']);
+    gulp.watch('resources/libs/*.js', ['scripts']);
+});
+
+/**
+ * Default Task
+ */
+gulp.task('default', ['styles','scripts','images','watch']);
+
